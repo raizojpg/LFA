96f88e7675b348cb2e64d2b157932923972410f8
raizojpg <116096714+raizojpg@users.noreply.github.com>
2024-03-22 01:24:16 +0200

    Update NFA.cpp

--- NFA.cpp
+++ NFA.cpp
@@ -3,27 +3,37 @@
 #include <string>
 #include <map>
 #include <set>
-#include <queue>
+#include <stack>
 
 std::ifstream in("input.txt");
 std::ofstream out("output.txt");
 
 int x, y, n, m, S, nrF, nrCuv;
 char l;
+std::string str;
+std::vector<int> states_name;
 std::set<int> final_states;
 std::map<int, std::map<char, std::vector<int>>> states;
-std::string str;
+
+std::map<int, std::vector<bool>> visited; 
+// This data strcuture helps us not repeating a search 
+//	that was already computed on a state at a specific point in time
+
 
 bool NFA(std::string& str) {
-	//I assumed that the lambda letters that may appear in our string are noted with '*'
+	// The lambda letters that may appear in our string are noted with '*'
 
 	//check for lambdas in our string
 	str.erase(remove(str.begin(), str.end(), '*'), str.end());
 	
-	std::queue<std::pair<int, int>> possible_states; //the second element will be the string index  
+	// We will use a stack, because once we found one possible path, 
+	//    we are no longer interested in following the search 
+	// If we would have used a queue, we would have computed all the possible searches 
+	
+	std::stack<std::pair<int, int>> possible_states; //the second element will be the string index  
 	possible_states.push({ S,0 });
 	while (!possible_states.empty()) {
-		std::pair<int, int> state_index = possible_states.front();
+		std::pair<int, int> state_index = possible_states.top();
 		possible_states.pop();
 		int state = state_index.first;
 		int index = state_index.second;
@@ -33,8 +43,11 @@ bool NFA(std::string& str) {
 				continue;
 			}
 			// multiple states are possible for each letter
-			for (auto& possible_state : states[state][str[index]]) {
-				possible_states.push({ possible_state,index + 1 });
+			for (int possible_state : states[state][str[index]]) {
+				if (visited[possible_state][index] == 0) {
+					visited[possible_state][index] = 1;
+					possible_states.push({ possible_state,index + 1 });
+				}
 			}
 		}
 		else if (index == str.length()) {
@@ -49,7 +62,7 @@ int main() {
 	in >> n;
 	for (int i = 0; i < n; i++) {
 		in >> x;
-		states[x];
+		states_name.push_back(x);
 	}
 	in >> m;
 	for (int i = 0; i < m; i++) {
@@ -67,6 +80,9 @@ int main() {
 	in >> nrCuv;
 	for (int i = 0; i < nrCuv; i++) {
 		in >> str;
+		for (int state : states_name) {
+			visited[state] = std::vector<bool>(str.length(), 0);
+		}
 		if (NFA(str)) { out << "DA" << std::endl; }
 		else { out << "NU" << std::endl; }
 	}
